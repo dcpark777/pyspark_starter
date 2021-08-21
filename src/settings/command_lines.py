1ae5483218bb7e54a40af57bc333ff3d5780e228
Dan Park <dan.park.primary@gmail.com>
2021-08-21 12:17:00 -0400

    add docstrings

--- src/settings/command_lines.py
+++ src/settings/command_lines.py
@@ -1,20 +1,20 @@
-import argparse
 from argparse import ArgumentParser
 
 
-class SparkCommandLine(ArgumentParser):
-    def __init__(self):
-        super(SparkCommandLine, self).__init__()
-        self.spark_executor_cores = self.add_argument('--spark_executor_cores', required=False, default=4)
-
-
 class BaseCommandLine(ArgumentParser):
-    """
-
-    """
+    """CommandLine parser for arguments required by all jobs."""
     def __init__(self):
         super(BaseCommandLine, self).__init__()
         self.job_name = self.add_argument('--job_name', required=True)
         self.environment = self.add_argument('--environment', required=True)
         self.region = self.add_argument('--region', required=True)
+
+
+class SparkCommandLine(ArgumentParser):
+    """CommandLine parser for optional spark configuration arguments"""
+    def __init__(self):
+        super(SparkCommandLine, self).__init__()
+        self.spark_executor_cores = self.add_argument('--spark_executor_cores', required=False, type=int, default=4)
+        self.spark_executor_memory = self.add_argument('--spark_executor_memory', required=False, type=str, default='15g')
+
 
